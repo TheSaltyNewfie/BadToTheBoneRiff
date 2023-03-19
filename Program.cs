1a3bafeb237347606b426982104dbb1d7e74cdae
Toby Dodge <tobydodge65@gmail.com>
2023-03-19 18:08:32 -0230

    Updated randomness and no longer shows terminal

--- Program.cs
+++ Program.cs
@@ -1,25 +1,41 @@
-﻿using System.Drawing.Printing;
-using System.Media;
+﻿using System.Media;
+using System.Runtime.InteropServices;
 
 namespace BadToTheBoneRiff
 {
     internal class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         static void Main(string[] args)
         {
+            AllocConsole();
+
+            IntPtr hwnd = FindWindow("ConsoleWindowClass", Console.Title);
+
+            ShowWindow(hwnd, 0);
+
             SoundPlayer player = new SoundPlayer(@"sound.wav");
 
             while(true)
             {
                 Random rand = new Random();
 
-                int index = rand.Next(0, 6942069);
+                int index = rand.Next(0, 6942069); // get random numbers
                 Console.WriteLine(index);
 
                 if (index == 1072842)
                 {
                     player.Play();
-                    Thread.Sleep(300000);
+                    int randtime = rand.Next(300000, 1500000); // 5-25 minutes wait time
+                    Thread.Sleep(randtime);
                 }
             }
         }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BadToTheBoneRiff
+
+## What is this?
+Just a simple joke program that plays the bad to the bone riff randomly, it runs in the background with no console and must be closed from task manager
