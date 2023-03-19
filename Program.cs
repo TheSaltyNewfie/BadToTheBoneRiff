ddd65581c08605b6c066fc57866138a923fbde2a
Toby Dodge <tobydodge65@gmail.com>
2023-03-19 03:24:17 -0230

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing.Printing;
+using System.Media;
+
+namespace BadToTheBoneRiff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer player = new SoundPlayer(@"sound.wav");
+
+            while(true)
+            {
+                Random rand = new Random();
+
+                int index = rand.Next(0, 6942069);
+                Console.WriteLine(index);
+
+                if (index == 1072842)
+                {
+                    player.Play();
+                    Thread.Sleep(300000);
+                }
+            }
+        }
+    }
+}
